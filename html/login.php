c7c69104ebb00a47be9989ccab3ce5782fe3d305
alriyadsouza <alriyatreeza929@gmail.com>
2023-08-21 17:57:19 +0530

    login errror fixed

--- html/login.php
+++ html/login.php
@@ -14,7 +14,7 @@
   	<?php include('errors.php'); ?>
   	<div class="input-group">
   		<label>Username</label>
-  		<input type="text" name="username" >
+  		<input type="text" name="username">
   	</div>
   	<div class="input-group">
   		<label>Password</label>
@@ -29,3 +29,48 @@
   </form>
 </body>
 </html>
+
+<?php
+// login.php
+
+if (isset($_POST['login_user'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Set your database credentials here
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "hivetohome"; // Update with your database name
+
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+    if ($conn->connect_error) {
+        die('Could not connect to the database.');
+    }
+
+    // Query to fetch user_id using username
+    $query = "SELECT user_id FROM users WHERE username = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $user_id = $row['user_id'];
+        
+        // Store user_id in session for future use
+        session_start();
+        $_SESSION['user_id'] = $user_id;
+        
+        // Redirect to order page or any other page you want
+        header('location: index.php');
+    } else {
+        echo "Login failed. Invalid username or password.";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
